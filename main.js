5dca971a75f021e184d87d30aa654553d7e0efff
Talal-94 <mito.gldr9@gmail.com>
2021-10-11 11:53:57 +0300

    4th

--- main.js
+++ main.js
@@ -13,12 +13,7 @@
         }
     }
     showMenu("nav-toggle", 'nav-menu')
-
-
-
-
     /* ====== remove menu mobile ====== */
-
     const navLink = document.querySelectorAll('.nav__link')
 
     function linkAction (){
@@ -27,18 +22,37 @@
         navMenu.classList.remove('show-menu')
     }
     navLink.forEach(n => n.addEventListener('click', linkAction))
-
    /* ====== scroll sections active link ====== */
-
-
-   /* ====== show menu ====== */
-
-
+   const sections = document.querySelectorAll('section[id]')
+   function scrollActive(){
+       const scrollY = window.pageYOffset
+   
+       sections.forEach(current =>{
+           const sectionHeight = current.offsetHeight
+           const sectionTop = current.offsetTop - 50;
+           sectionId = current.getAttribute('id')
+   
+           if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+               document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+           }else{
+               document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+           }
+       })
+   }
+   window.addEventListener('scroll', scrollActive)
     /* ====== cange background header ====== */
-
-
-    // when the scroll is greater than 200 vh, add scroll header class
-
+   function scrollHeader() {
+       const nav = document.getElementById('header')
+       // when the scroll is greater than 200 vh, add scroll header class to header tag
+       if(this.scrollT .= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    }
+window.addEventListener('scroll', scrollHeader)
     /* ====== show scroll top ====== */
-
+    function scrollTop(){
+        const scrollTop = document.getElementById('scroll-top');
+        // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+        if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('show-scroll')
+    }
+    window.addEventListener('scroll', scrollTop)
+    
     /* ====== show menu ====== */
